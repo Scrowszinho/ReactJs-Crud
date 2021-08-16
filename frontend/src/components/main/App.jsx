477323f2afc06f7487dd82c4fd32d827d9e53480
Luis Gustavo <gustascrows@gmail.com>
2021-08-16 16:10:46 -0300

    App reissued

--- frontend/src/components/main/App.jsx
+++ frontend/src/components/main/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import Footer from '../layout/Footer';
+import Footer from '../layout/footer/Footer';
+import Header from '../layout/header/Header';
 import Main from '../layout/Main';
-import Menu from '../layout/Menu';
 import './App.css';
 
 function App(props){
     return(
         <div className='App'>
-             <Main title={'Home'} subTitle={'This is a project home'} />
-             <Menu />
-             <Footer />
+            <Header title={'Home'} subTitle={'Test subtitle'}/>
+            <Main>
+                <h1>Welcome</h1>
+                <hr />
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    Necessitatibus provident at nihil molestiae temporibus illum consectetur deleniti doloribus magni ex est rem,
+                     reprehenderit doloremque, totam ipsum praesentium ut a voluptatibus.</p>
+            </Main>
         </div>
     );
 }
